0d06e214d950e59c3f3bcd3795e69fa609683abb
DHANUSHKODI L <dhnanushkodilax@gmail.com>
2026-01-19 21:54:39 +0530

    IEmployeeRepository

--- EFTicketPortalLibrary/Repos/IEmployeeRepository.cs
+++ EFTicketPortalLibrary/Repos/IEmployeeRepository.cs
@@ -6,9 +6,8 @@ namespace EFTicketPortalLibrary.Repos;
 public interface IEmployeeRepository
 {
     Task AddEmployeeAsync(Employee employee);
-        Task UpdateEmployeeAsync(Employee employee);
-        Task DeleteEmployeeAsync(string employeeId);
-
-        Task<Employee> GetEmployeeByIdAsync(string employeeId);
-        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+    Task UpdateEmployeeAsync(Employee employee);
+    Task DeleteEmployeeAsync(string employeeId);
+    Task<Employee> GetEmployeeByIdAsync(string employeeId);
+    Task<IEnumerable<Employee>> GetAllEmployeesAsync();
 }
