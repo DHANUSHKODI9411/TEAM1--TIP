212d8f1691cc4e09bbb1b989568187e45ef89646
Vivek D K <vivekdk9141959@gmail.com>
2026-01-19 21:23:29 +0530

    Refactor TicketReplies model to improve attribute annotations and remove merge conflict markers

--- EFTicketPortalLibrary/Models/TicketReplies.cs
+++ EFTicketPortalLibrary/Models/TicketReplies.cs
@@ -11,78 +11,38 @@ namespace EFTicketPortalLibrary.Models;
  
 [Table("TicketReplies")]
 public class TicketReplies
-
 {
-
     [Key]
-
     [Column(TypeName = "char(5)")]
-
     [Required(ErrorMessage = "ReplyId is required...")]
-
     [MaxLength(5, ErrorMessage = "Reply Id must be exactly 5 characters only.")]
-
     public string? ReplyId { get; set;} = null!;
 
-
-
- 
-
     [Column(TypeName = "char(5)")]
-
     [Required(ErrorMessage = "TicketId is required...")]
-
     [MaxLength(5, ErrorMessage = "Ticket Id must be exactly 5 characters only.")]
-
     public string? TicketId { get; set; } = null!;
 
-
-
-   
-
     [Column(TypeName = "char(5)")]
-
     [Required(ErrorMessage = "EmployeeId is required...")]
-
     [MaxLength(5, ErrorMessage = "Employee Id must be exactly 5 characters only.")]
-
     public string? CreatedEmployeeId { get; set;} = null!;
 
-
-
     [Column(TypeName = "char(5)")]
-
     [Required(ErrorMessage = "Assgned EmployeeId is required...")]
-
     [MaxLength(5, ErrorMessage = "AssignedEmployee Id must be exactly 5 characters only.")]
-
-
     public string? AssignedEmployeeId {get; set;} = null!;
 
-
-
- 
-
     [Column(TypeName = "nvarchar(max)")]
-
     [Required(ErrorMessage = "Please reply for the text...")]
-
     public string? ReplyText { get; set;} = null!;
 
-
     [Required(ErrorMessage = "Please select the date...")]
-
     public DateTime RepliedDate { get; set;}
 
-[ForeignKey(nameof(TicketId))]
+    [ForeignKey(nameof(TicketId))]
     [InverseProperty("Replies")]
     public virtual Ticket Ticket { get; set; } = null!;
-<<<<<<< HEAD
-    [ForeignKey("CreatedEmployeeId")]
-    public virtual Employee CreatedEmployee { get; set; } = null!;
-    [ForeignKey("AssignedEmployeeId")]
-    public virtual Employee AssignedEmployee { get; set; } = null!;
-=======
 
     [ForeignKey(nameof(CreatedEmployeeId))]
     [InverseProperty("CreatedEmpReplies")]
@@ -92,10 +52,6 @@ public class TicketReplies
     [InverseProperty("AssignedEmpReplies")]
     public virtual Employee? AssignedEmployee { get; set; }
 }
-
-
->>>>>>> ee80a03c557fd579b88b8184b3b849a7c8184432
- 
 
  
 
