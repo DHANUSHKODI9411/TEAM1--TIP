04a4b1a499935e2a15557a941a187af97539b30b
Vivek D K <vivekdk9141959@gmail.com>
2026-01-19 17:06:37 +0530

    Edited Status.cs and StatusRepository

--- EFTicketPortalLibrary/Repos/StatusRepository.cs
+++ EFTicketPortalLibrary/Repos/StatusRepository.cs
@@ -1,8 +1,33 @@
 using System;
+using EFTicketPortalLibrary.Models;
 
 namespace EFTicketPortalLibrary.Repos;
 
-public class StatusRepository
+public class StatusRepository : IStatusRepository
 {
+    EYInstituteDBContext context = new EYInstituteDBContext();
+    public async Task AddStatusAsync(Status status)
+    {
+        throw new NotImplementedException();
+    }
 
+    public async Task DeleteStatusAsync(string StatusId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Status>> GetAllStatussAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Status> GetStatusAsync(string StatusId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateStatusAsync(string StatusId, Status status)
+    {
+        throw new NotImplementedException();
+    }
 }
