82bcb2b9abcfaec67729e23b204e4e9b0c852020
Vivek D K <vivekdk9141959@gmail.com>
2026-01-19 09:20:24 +0530

    Created Models and Repos folder

--- EFTicketPortalLibrary/obj/Debug/net10.0/EFTicketPortalLibrary.AssemblyInfo.cs
+++ EFTicketPortalLibrary/obj/Debug/net10.0/EFTicketPortalLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1374afff79726940d896297e904924f96857df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ad44d213929c40b9ce43dc9f6bc3faf93f9266")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
