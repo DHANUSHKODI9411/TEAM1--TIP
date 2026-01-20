6c413b58fcb8cb58bd3cf8766c8d6d5c7456a1f1
karanchrish <karanchrish@gmail.com>
2026-01-20 10:05:37 +0530

    added swagger package

--- TicketPortalWebApi/obj/Debug/net10.0/TicketPortalWebApi.AssemblyInfo.cs
+++ TicketPortalWebApi/obj/Debug/net10.0/TicketPortalWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketPortalWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3542aac690a4d4d81b8757b66c4620acea42a2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab38b9d95f43c5d8d4b6913ebaa00667050abcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketPortalWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketPortalWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
