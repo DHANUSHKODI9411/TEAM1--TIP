2cd7d841efa9b5c57a49b18e3f318d2d7dee9633
Vivek D K <vivekdk9141959@gmail.com>
2026-01-20 09:38:50 +0530

    Update project.nuget.cache to fix path issues and add expected package files

--- EFTicketPortalLibrary/obj/Debug/net10.0/EFTicketPortalLibrary.AssemblyInfo.cs
+++ EFTicketPortalLibrary/obj/Debug/net10.0/EFTicketPortalLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f340f6b520809fce12569b426afa38dbc62d0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8054169343502d9381207692f0a4df93de6f82df")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFTicketPortalLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
