f67c7c505e4438fc6aea1529b837b42ae15692c2
Vivek D K <vivekdk9141959@gmail.com>
2026-01-20 14:33:45 +0530

    Refactor SLA model: clean up whitespace and adjust TicketType property

--- EFTicketPortalLibrary/Models/SLA.cs
+++ EFTicketPortalLibrary/Models/SLA.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace EFTicketPortalLibrary.Models;
+
 [Table("SLA")]
 public class SLA
 {
@@ -11,27 +12,24 @@ public class SLA
     [Column(TypeName = "char(5)")]
     [MaxLength(5, ErrorMessage = "SLA ID must be exactly 5 characters")]
     public string SLAid { get; set; } = null!;
- 
+
     [Required(ErrorMessage = "Ticket Type ID is required")]
     [Column(TypeName = "char(5)")]
     [MaxLength(5, ErrorMessage = "Ticket Type ID must be exactly 5 characters")]
     [ForeignKey("TicketType")]
     public string TicketTypeId { get; set; } = null!;
- 
+
     [Required(ErrorMessage = "Response time is required")]
     [Range(1, 8760, ErrorMessage = "Response time must be between 1-8760 hours")]
     public int ResponseTime { get; set; }
- 
+
     [Required(ErrorMessage = "Resolution time is required")]
     [Range(1, 8760, ErrorMessage = "Resolution time must be between 1-8760 hours")]
     public int ResolutionTime { get; set; }
- 
+
     [Column(TypeName = "NVARCHAR(500)")]
     [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
     public string? Description { get; set; }
- 
-    [ForeignKey(nameof(TicketTypeId))]
-    [InverseProperty("Slas")]
-    public virtual TicketType TicketType { get; set; } = null!;
-}
 
+    public virtual TicketType? TicketType { get; set; }
+}
