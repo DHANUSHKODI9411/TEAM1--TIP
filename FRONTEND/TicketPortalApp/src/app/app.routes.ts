c936e96f39c710fd25af397a6b9eaf00cae104d9
Vivek D K <vivekdk9141959@gmail.com>
2026-01-21 17:14:37 +0530

    Add StatusComponent to app structure and routing

--- FRONTEND/TicketPortalApp/src/app/app.routes.ts
+++ FRONTEND/TicketPortalApp/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { TicketTypeComponent } from './tickettype-component/tickettype-component';
+import { StatusComponent } from './status-component/status-component';
 
-export const routes: Routes = [{path: 'tickettype',component:TicketTypeComponent}];
+export const routes: Routes = [
+  { path: 'tickettype',component:TicketTypeComponent},
+  { path: 'status', component: StatusComponent }
+];
--- FRONTEND/TicketPortalApp/src/app/app.ts
+++ FRONTEND/TicketPortalApp/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TicketTypeComponent } from './tickettype-component/tickettype-component';
+import { StatusComponent } from './status-component/status-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,TicketTypeComponent],
+  imports: [RouterOutlet, TicketTypeComponent, StatusComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
