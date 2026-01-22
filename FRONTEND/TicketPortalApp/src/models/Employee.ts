561da547e902a810780abfb84c377bf26c39c160
Vivek D K <vivekdk9141959@gmail.com>
2026-01-22 15:40:41 +0530

    refactor: Simplify Employee model by initializing properties and updating constructor

--- FRONTEND/TicketPortalApp/src/models/Employee.ts
+++ FRONTEND/TicketPortalApp/src/models/Employee.ts
@@ -1,18 +1,19 @@
+export class Employee {
+  public employeeId: string = '';
+  public employeeName: string = '';
+  public email: string = '';
+  public password: string = '';
+  public role: string = 'User';
 
-export class Employee{
-    public employeeId:string;
-    public employeeName:string;
-    public email:string;
-    public password:string;
-    public role: string;
-
-    constructor(employeeId:string,employeeName:string,email:string,password:string,role:string){
-        this.employeeId=employeeId;
-        this.employeeName=employeeName;
-        this.email=email;
-        this.password= password;
-        this.role=role;
-    }
-   
-
+  constructor(employeeId: string = '',
+              employeeName: string = '',
+              email: string = '',
+              password: string = '',
+              role: string = 'User') {
+    this.employeeId = employeeId;
+    this.employeeName = employeeName;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
 }
